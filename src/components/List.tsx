e36c899bdd2c27cdea8b11b7aff3ac73c9564cf7
inakarune <inakarune@naver.com>
2020-06-25 07:49:48 +0900

    'delete'

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,24 +11,20 @@ interface PropsFromApp {
 }
 class List extends React.Component<PropsFromApp> {
 
-    private selectList = (name: string, idx: number): void => {
-        this.props.selectSong(name, idx);
-    }
-
-    render() {
+    render(): JSX.Element {
         return (
             <div className="list-container">
                 <h1>Music</h1>
                 <div className="list-box">
                     <ul className="song-list">
                         { this.props.list.map((item: any, idx: number) => 
-                        <li key={ idx } className={ this.props.index === idx ? 'on' : '' } onClick={ () => this.selectList(item.name, idx) }>
+                        <li key={ idx } className={ this.props.index === idx ? 'on' : '' } onClick={ () => this.props.selectSong(item.name, idx) }>
                             <span className="thumnail-img"><FontAwesomeIcon icon={ faMusic } size="1x" color="white"/></span>
                             <p className="song-name">
                                 { item.name }<br/>
                                 <span className="author">Unknown</span>
                             </p>
-                            <span onClick={ () => this.props.deleteSong(item.name, idx) }className={ this.props.index === idx ? 'trash-on' : 'trash-off' }><FontAwesomeIcon icon={ faTrashAlt } size="1x" color="white"/></span>
+                            <span onClick={ () => this.props.deleteSong(item.name, idx) } className={ this.props.index === idx ? 'trash-on' : 'trash-off' }><FontAwesomeIcon icon={ faTrashAlt } size="1x" color="white"/></span>
                         </li>)
                         }
                     </ul>
